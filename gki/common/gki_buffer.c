ff5cd5734ede29dcbe603ad96fa3ed9e0693ce90
Pavlin Radoslavov <pavlin@google.com>
2015-09-23 10:08:20 -0700

    GKI cleanup - Eliminate function GKI_get_pool_bufsize()
    Conflicts:
    	gki/common/gki_buffer.c
    
    Change-Id: I87168157b9bb3e9d315c03b98a10d18b38cce3e1

--- gki/common/gki_buffer.c
+++ gki/common/gki_buffer.c
@@ -476,22 +476,3 @@ UINT16 GKI_poolcount (UINT8 pool_id)
 
     return (gki_cb.com.freeq[pool_id].total);
 }
-
-/*******************************************************************************
-**
-** Function         GKI_get_pool_bufsize
-**
-** Description      Called by an application to get the size of buffers in a pool
-**
-** Parameters       Pool ID.
-**
-** Returns          the size of buffers in the pool
-**
-*******************************************************************************/
-UINT16 GKI_get_pool_bufsize (UINT8 pool_id)
-{
-    if (pool_id < GKI_NUM_TOTAL_BUF_POOLS)
-        return (gki_cb.com.freeq[pool_id].size);
-
-    return (0);
-}
